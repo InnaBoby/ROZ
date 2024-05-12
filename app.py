d71c85dc0220ca4f62562944908bee2fa03e2adc
InnaBoby <146350138+InnaBoby@users.noreply.github.com>
2024-05-12 23:24:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from pyaspeller import YandexSpeller
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, BertTokenizer, BertForSequenceClassification
 from func import *
 
 
